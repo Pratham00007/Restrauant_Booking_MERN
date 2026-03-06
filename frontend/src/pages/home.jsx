55b9a9dd3fe1a01ea993cd74f1895bb4f880c485
Pratham <pratham10600@gmail.com>
2026-03-06 23:17:55 +0530

    added

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import About from '../components/About'
+import Qualities from '../components/Qualities'
+import Menu from '../components/Menu'
+import WhoAreWe from '../components/WhoAreWe'
 
 const Home = () => {
   return (
@@ -8,6 +11,9 @@ const Home = () => {
       <>
       <HeroSection/>
       <About/>
+      <Qualities/>
+      <Menu/>
+      <WhoAreWe/>
       </>
     
   )
