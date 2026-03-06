903a732ae6d1d6bb997440d87372090c756deabd
Pratham <pratham10600@gmail.com>
2026-03-07 02:02:38 +0530

    completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+            <div className="left">Rasuu</div>
+            <div className="right">
+                <p>G-Floor Rani Mall, America</p>
+                <p>Open: 05:00 AM - 09:00 PM</p>
+            </div>
+            </div>
+            <div className="banner">
+                <div className="left">
+                    <p>Developed By RasuuDaDahabaa</p>
+                </div>
+                <div className="right">
+                    <p>All Rights reserved By RasuuDaDhabaa</p>
+                </div>
+            </div>
+       
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
